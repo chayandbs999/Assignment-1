cf1dee5a1550331a7d706c4ec9276cc392e24bba
chayandbs999 <109010876+chayandbs999@users.noreply.github.com>
2022-07-16 14:42:45 +0530

    Add files via upload

---
+++ Question-12.cpp
@@ -0,0 +1,10 @@
+// 12. Find the output of the below code
+#include<stdio.h>
+int main()
+{
+    int x=printf("ineuron");
+    printf("%d",x);
+    return 0;
+}
+
+// Output -> ineuron7
---
+++ Question-4.cpp
@@ -0,0 +1,15 @@
+/*
+4. WAP to find the area of the circle. Take radius of circle from user as input and print the result in below given format.
+
+Expected output format – “Area of circle is A having the radius R”. Replace A with area & R with radius.
+*/
+#include<stdio.h>
+int main(){
+    float A,R;
+    printf("Enter the length of redius o f circle=");
+    scanf("%f",&R);
+    A = 3.14*R*R;
+    printf("Area of circle is %f having the radius %f",A,R);
+    return 0;
+
+}
